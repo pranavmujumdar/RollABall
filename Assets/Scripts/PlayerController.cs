8b40701c1caf9f4fa7862ff804e030b6c5914309
Pranav Mujumdar <psmujumdar@gmail.com>
2020-01-12 12:20:43 -0500

    Basic Template for roll a ball created

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+    // fixedupdate is called just before performing physics calculations, physics code goes here
+    private Rigidbody rigidbody;
+    public Text scoreText;
+    public Text winText;
+    private int score; 
+    public float speed; 
+    void Start() 
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        score = 0;
+        UpdateScore();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidbody.AddForce (movement * speed);
+    }
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            UpdateScore();
+        }
+    }
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if(score >= 17)
+        {
+            winText.text = "You Win!";
+        }
+        
+    }
+}

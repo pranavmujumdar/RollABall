c37ea8611b4cc69f6eaf6e06014e980c1c9b494d
Pranav Mujumdar <psmujumdar@gmail.com>
2020-01-15 13:59:18 -0500

    added height variable to NewY to manipulate how high the spik will go :zap:

--- Assets/Scripts/SpikeMotion.cs
+++ Assets/Scripts/SpikeMotion.cs
@@ -7,15 +7,13 @@ public class SpikeMotion : MonoBehaviour
     //adjust this to change speed
     float speed = 1f;
     //adjust this to change how high it goes
-    float height = 0.5f;
+    float height = 0.3f;
 
     // Update is called once per frame
     void Update()
     {
-        //get the objects current position and put it in a variable so we can access it later with less code
-        //Vector3 pos = transform.position;
         //calculate what the new Y position will be
-        float newY = Mathf.Sin(Time.time * speed);
+        float newY = Mathf.Sin(Time.time * speed) * height;
         //set the object's Y to the new calculated Y
         transform.position = new Vector3(transform.position.x, newY, transform.position.z) ;
     }
